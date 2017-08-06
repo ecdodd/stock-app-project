0fdd547f1a88148b62925d9f8a73f4c8e8f206e9
ECDODD <ecd339@nyu.edu>
2017-08-06 18:46:28 -0400

    Define stock functions and utilize control flow.

--- app/stock.py
+++ app/stock.py
@@ -35,3 +35,32 @@ operation_menu = """
 
 input_operation = input(operation_menu)
 operation = input_operation.lower()
+
+# COMPILE REQUEST PARAMETERS
+
+data_source = 'google'
+start = str(date.today() - timedelta(days=15))
+end = str(date.today())
+response = data.DataReader(symbol, data_source, start, end)
+
+# DEFINE FUNCTIONS
+
+def display_stocks(response):
+    response["Symbol"] = symbol
+    columns = ["Symbol", "Open", "High", "Low", "Close", "Volume"]
+    response = response[columns]
+    print(response)
+    print("Source: " + (data_source))
+
+def add_stocks(response):
+    response["Symbol"] = symbol
+    columns = ["Symbol", "Open", "High", "Low", "Close", "Volume"]
+    response = response[columns]
+    response.to_csv(csv_file_path, mode = 'a', header=False)
+    print("Thank you! Your response has been saved.")
+
+# CONTROL FLOW
+
+if operation == "display": display_stocks(response)
+elif operation == "add": add_stocks(response)
+else: print("Error. Please choose one of the recognized operations.")
