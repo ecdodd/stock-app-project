b4593414c2b3b1efea9eb73f569b9d7c8640bbc1
ECDODD <ecd339@nyu.edu>
2017-08-06 18:45:49 -0400

    Create stock menu operations.

--- app/stock.py
+++ app/stock.py
@@ -1,1 +1,37 @@
+import csv
+from pandas_datareader import data
+from datetime import date, timedelta
+from IPython import embed
 
+# READ STOCKS CSV
+
+stocks = []
+
+csv_file_path = "data/stocks.csv"
+
+# DISPLAY MENU #1
+
+stock_menu = """
+------------------------------------------------------
+    STOCK APPLICATION
+------------------------------------------------------
+    Welcome to the stock app!
+
+    Enter stock symbol: """
+
+stock_symbol = input(stock_menu)
+symbol = stock_symbol
+
+# DISPLAY MENU #2
+
+operation_menu = """
+
+    Operation | Description
+    --------- |-------------
+    'display' | Display a range of stock values.
+    'add'     | Add a range of stock values to a file.
+
+    Enter operation: """
+
+input_operation = input(operation_menu)
+operation = input_operation.lower()
